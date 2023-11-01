6384e721a6ca8aea9cee21b48282fca796ae1e5a
chenchengming <chenchengming@yy.com>
2023-11-01 21:24:37 +0800

    项目搭建

---
+++ Class/TestSPMObject.m
@@ -0,0 +1,16 @@
+//
+//  TestSPMObject.m
+//  
+//
+//  Created by 陈成明 on 2023/11/1.
+//
+
+#import "TestSPMObject.h"
+
+@implementation TestSPMObject
+
+- (void)test {
+    NSLog(@"123");
+}
+
+@end
---
+++ Class/include/TestSPMObject.h
@@ -0,0 +1,18 @@
+//
+//  TestSPMObject.h
+//  
+//
+//  Created by 陈成明 on 2023/11/1.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestSPMObject : NSObject
+
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
